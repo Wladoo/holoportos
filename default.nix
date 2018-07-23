2bbda6794f3e4ed21a6a6f822621d75c093d875c
Franz Pletz <fpletz@fnordicwalking.de>
2018-07-23 13:11:16 +0200

    reindent

--- default.nix
+++ default.nix
@@ -15,38 +15,38 @@ in
 
 {
   iso = (import "${nixpkgs}/nixos/lib/eval-config.nix" {
-      inherit system;
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+    inherit system;
+    modules = [
+      "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
 
-        # These modules are needed to inject the correct nixos version from Hydra
-        ./modules/base.nix
-        ./modules/version.nix
+      # These modules are needed to inject the correct nixos version from Hydra
+      ./modules/base.nix
+      ./modules/version.nix
 
-        # The custom config for our install iso
-        ({ pkgs, ... }: {
-          isoImage.isoBaseName = "holoportos";
-          holoport.versionSuffix = ".${toString holoport.revCount}.${holoport.shortRev}";
-          holoport.revision = holoport.rev or holoport.shortRev;
+      # The custom config for our install iso
+      ({ pkgs, ... }: {
+        isoImage.isoBaseName = "holoportos";
+        holoport.versionSuffix = ".${toString holoport.revCount}.${holoport.shortRev}";
+        holoport.revision = holoport.rev or holoport.shortRev;
 
-          system.nixos.versionSuffix = nixpkgsVersionSuffix;
-          system.nixos.revision = nixpkgs.rev or nixpkgs.shortRev;
+        system.nixos.versionSuffix = nixpkgsVersionSuffix;
+        system.nixos.revision = nixpkgs.rev or nixpkgs.shortRev;
 
-          # The NIXOS_CONFIG variable will be used by nixos-install on the so
-          # as the configuration of the system to install. This configuration
-          # will first import the actual configuration.nix that was generated
-          # with nixos-generated-config on the target fs mount on /mnt.
-          # Additionally, all our custom modules will be imported so our
-          # configuration is applied.
-          environment.variables.NIXOS_CONFIG =
-            toString (pkgs.writeText "holoport-configuration.nix" ''
-              {
-                imports = [ "/mnt/etc/nixos/configuration.nix" ]
-                  ++ (import "${pkgs.holoportModules}/modules/module-list.nix");
-              }
-            '');
-        })
-      ];
+        # The NIXOS_CONFIG variable will be used by nixos-install on the so
+        # as the configuration of the system to install. This configuration
+        # will first import the actual configuration.nix that was generated
+        # with nixos-generated-config on the target fs mount on /mnt.
+        # Additionally, all our custom modules will be imported so our
+        # configuration is applied.
+        environment.variables.NIXOS_CONFIG =
+          toString (pkgs.writeText "holoport-configuration.nix" ''
+            {
+              imports = [ "/mnt/etc/nixos/configuration.nix" ]
+                ++ (import "${pkgs.holoportModules}/modules/module-list.nix");
+            }
+          '');
+      })
+    ];
     }).config.system.build.isoImage;
 
   channels.nixpkgs = import "${nixpkgs}/nixos/lib/make-channel.nix" {
