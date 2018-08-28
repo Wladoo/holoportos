3722ccdfa54ab9707f0c4e8ed813bfba48e4cbd5
Sam Rose <sam.rose@holo.host>
2018-08-28 17:05:17 -0400

    update groups for holoport user

--- modules/user.nix
+++ modules/user.nix
@@ -6,4 +6,15 @@
     extraGroups = [ "wheel" ];
   };
   services.mingetty.autologinUser = "holoport";
+  security.sudo.wheelNeedsPassword = false;
+  security.sudo.configFile =
+    ''
+      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
+      Defaults:root,%wheel env_keep+=NIX_PATH
+      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+      Defaults env_keep+=SSH_AUTH_SOCK
+      Defaults lecture = never
+      holoport ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
+    '';
+
 }
