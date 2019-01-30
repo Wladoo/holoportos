32ab40993a287035bfdb88624625888cea65e9a7
samrose <samuel.rose@gmail.com>
2019-01-30 17:40:26 -0500

    fix url

--- modules/base.nix
+++ modules/base.nix
@@ -19,7 +19,7 @@ in
     holoport.channels.nixpkgs = mkOption {
       type = types.str;
       # FIXME: final url
-      default = "http://holoportbuild.holo.host/job/holoportOS-testnet/master/channels.nixpkgs/latest/download/1";
+      default = "http://holoportbuild.holo.host/job/holoportOs-testnet/testnet/channels.nixpkgs/latest/download/1";
       description = ''
         URL understood by Nix to a nixpkgs/NixOS channel
       '';
@@ -28,7 +28,7 @@ in
     holoport.channels.holoport = mkOption {
       type = types.str;
       # FIXME: final url
-      default = "http://holoportbuild.holo.host/job/holoportOS-testnet/master/channels.holoport-testnet/latest/download/1";
+      default = "http://holoportbuild.holo.host/job/holoportOs-testnet/testnet/channels.holoport-testnet/latest/download/1";
       description = ''
         URL understood by Nix to the Holoport channel
       '';
