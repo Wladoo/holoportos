4a72a94696ca72aa6eb09f72449003f8f8442048
samrose <samuel.rose@gmail.com>
2019-02-04 16:53:03 -0500

    holochain rust deps packaging

--- overlay.nix
+++ overlay.nix
@@ -16,5 +16,5 @@ in
 
 
   holoport-cloudflared = callPackage ./modules/holoport-cloudflared/cloudflared.nix {};
-  holochain = callPackage ./modules/holoport-rust/default.nix {};
+  holochain = callPackage ./modules/holochain-rust/default.nix {};
 }
