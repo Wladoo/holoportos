9a3f1f8d6ddf66a7de46b47d9b4cb9f31298b9e6
samrose <samuel.rose@gmail.com>
2019-02-04 16:56:21 -0500

    holochain rust deps packaging

--- modules/holochain-rust/default.nix
+++ modules/holochain-rust/default.nix
@@ -1,9 +1,9 @@
 #{ nixpkgs ? import <nixpkgs> {} }:
 #with nixpkgs;
-{stdenv, fetchurl, builtins}:
+{stdenv, fetchurl, fetchTarball}:
 let
   #inherit stdenv fetchurl;
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  moz_overlay = import (fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> {
     overlays = [ moz_overlay ];
   };
