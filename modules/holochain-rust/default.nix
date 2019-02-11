ea0d5f959f68375d28ca964a76cfb4ad65a70621
samrose <samuel.rose@gmail.com>
2019-02-11 12:54:48 -0500

    refactor rust and nodejs for hc core rm inherit

--- modules/holochain-rust/default.nix
+++ modules/holochain-rust/default.nix
@@ -1,5 +1,4 @@
 let
-  inherit stdenv fetchurl;
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> {
     overlays = [ moz_overlay ];
