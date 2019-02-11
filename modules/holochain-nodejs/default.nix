f46ecbaed7bfbd5e9f70c9d788037ee6ca79a4a8
samrose <samuel.rose@gmail.com>
2019-02-11 17:20:41 -0500

    just nodejs

--- modules/holochain-nodejs/default.nix
+++ modules/holochain-nodejs/default.nix
@@ -10,17 +10,17 @@ let
   #date = "2019-01-24";
   #wasmTarget = "wasm32-unknown-unknown";
   #rust-build = (nixpkgs.rustChannelOfTargets "nightly" date [ wasmTarget ]);
-  nixpkgs = import <nixpkgs> {};
-  nodejs-8_13 = nixpkgs.nodejs-8_x.overrideAttrs(oldAttrs: rec {
+  #nixpkgs = import <nixpkgs> {};
+  nodejs-8_13 = pkgs.nodejs-8_x.overrideAttrs(oldAttrs: rec {
     name = "nodejs-${version}";
     version = "8.13.0";
-    src = nixpkgs.fetchurl {
+    src = pkgs.fetchurl {
       url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
       sha256 = "1qidcj4smxsz3pmamg3czgk6hlbw71yw537h2jfk7iinlds99a9a";
     };
   });
 in
-with nixpkgs;
+with pkgs;
 stdenv.mkDerivation rec {
 
  name = "holochain-nodejs-environment";
