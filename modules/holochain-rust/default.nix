ddfa2191d1c02d91c0785c0d578320c9c8197af3
samrose <samuel.rose@gmail.com>
2019-02-11 12:53:37 -0500

    refactor rust and nodejs for hc core

--- modules/holochain-rust/default.nix
+++ modules/holochain-rust/default.nix
@@ -1,5 +1,3 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-with nixpkgs;
 let
   inherit stdenv fetchurl;
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
@@ -9,11 +7,7 @@ let
 
   date = "2019-01-24";
   wasmTarget = "wasm32-unknown-unknown";
-in
-{
-
-
-  rust-build = (nixpkgs.rustChannelOfTargets "nightly" date [ wasmTarget ]);
+    rust-build = (nixpkgs.rustChannelOfTargets "nightly" date [ wasmTarget ]);
 
   nodejs-8_13 = nixpkgs.nodejs-8_x.overrideAttrs(oldAttrs: rec {
     name = "nodejs-${version}";
@@ -23,5 +17,24 @@ in
       sha256 = "1qidcj4smxsz3pmamg3czgk6hlbw71yw537h2jfk7iinlds99a9a";
     };
   });
+in
+with nixpkgs;
+stdenv.mkDerivation rec {
 
+ name = "holochain-rust-environment";
+
+  buildInputs = [
+
+    # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md
+    binutils gcc gnumake openssl pkgconfig coreutils
+
+    cmake
+    python
+    pkgconfig
+    rust-build
+
+    nodejs-8_13
+    yarn
+    zeromq4
+   ];
 }
