66b823abbe5dcff86f4db73ff48360d69ace26dd
samrose <samuel.rose@gmail.com>
2019-02-13 15:51:04 -0500

    holoport-rust

--- overlay.nix
+++ overlay.nix
@@ -16,5 +16,5 @@ in
 
 
   holoport-cloudflared = callPackage ./modules/holoport-cloudflared/cloudflared.nix {};
-  holoport-rust = callPackage ./modules/holoport-rust.nix {};
+  holoport-rust = callPackage ./packages/holoport-rust.nix {};
 }
