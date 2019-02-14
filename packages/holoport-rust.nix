7ed2a4b38da976b57fa81051ef745ade38648d96
samrose <samuel.rose@gmail.com>
2019-02-14 17:20:47 -0500

    holochain-rust

--- packages/holoport-rust.nix
+++ packages/holoport-rust.nix
@@ -1,26 +1,25 @@
-{ stdenv,
-  fetchFromGitHub,
-  pkgs ? (
-  let
-    pkgs = import <nixpkgs>;
-    pkgs_ = (pkgs {});
-    rustOverlay = (pkgs_.fetchFromGitHub {
-      owner = "mozilla";
-      repo = "nixpkgs-mozilla";
-      rev = "1608d31f7e5b2415fb80b5d76f97c009507bc45f";
-      sha256 = "0mznf82k7bxpjyvigxvvwpmi6gvg3b30l58z36x192q2xxv47v1k";
-    });
-  in (pkgs {
+{ pkgs
+, system ? builtins.currentSystem
+, fetchFromGitHub ? (pkgs {}).fetchFromGitHub
+, fetchurl ? (pkgs {}).fetchurl
+, rustOverlay ? fetchFromGitHub {
+    owner  = "mozilla";
+    repo   = "nixpkgs-mozilla";
+    rev    = "7e54fb37cd177e6d83e4e2b7d3e3b03bd6de0e0f";
+    sha256 = "1shz56l19kgk05p2xvhb7jg1whhfjix6njx1q4rvrc5p1lvyvizd";
+  }
+}:
+pkgs {
     overlays = [
 
       (import (builtins.toPath "${rustOverlay}/rust-overlay.nix"))
 
-      (self: super: { rustRegistry = super.callPackage ./rust-packages.nix { }; }) # by adding the rustRegistry entry here in the overlay, the specific rustRegistry date / SHA will be used in the later overlays and compiles.
+      #(self: super: { rustRegistry = super.callPackage ./rust-packages.nix { }; }) # by adding the rustRegistry entry here in the overlay, the specific rustRegistry date / SHA will be used in the later overlays and compiles.
 
       (self: super:
       let
         # base = super.rustChannels.nightly;
-        base = super.rustChannelOf { date = "2019-01-24"; channel = "nightly"; };
+        base = super.rustChannelOfTargets { date = "2019-01-24"; channel = "nightly"; target = "wasm32-unknown-unknown"; };
       in
       {
         rust = {
@@ -33,27 +32,26 @@
         });
       })
     ];
-  }))
-}:
 
-pkgs.rustPlatform.buildRustPackage rec {
-  name = "holochain-rust-${version}";
+pkgs.rustPlatform.buildRustPackage = rec {
+  name = "hello_world";
   version = "0.0.1";
 
-  src = fetchFromGitHub {
-    owner = "holochain";
-    repo = "holochain-rust";
-    rev = "e761215a3e8a384f98b43f3d09e42dc5c1f628a4";
-    sha256 = "19n5njdyxif58vp84w1xl0b212xxlag6mv4q5plpqbg5a4xpbscd";
+  src = pkgs.fetchFromGitHub {
+    owner = "samrose";
+    repo = "hello_world";
+    rev = "082775d0388552e32b644a1e3da8755a5f5e8c6a";
+    sha256 = "1jhkhpyh8g0ja6rr8bffphybhz53m7fav6r8pndv94msjdjdc7f6";
   };
+  buildInputs = [ pkgs.rustc pkgs.cargo ];
+  cargoSha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
 
-  cargoSha256 = "0q68qyl2h6i0qsz82z840myxlnjay8p1w5z7hfyr8fqp7wgwa9cx";
-
-  meta = with stdenv.lib; {
-    description = "Holochain Rust";
-    homepage = https://github.com/holochain/holochain-rust;
+  meta = with pkgs.stdenv.lib; {
+    description = "a test";
+    homepage = https://github.com/samrose/hello_world;
     license = licenses.unlicense;
     maintainers = [ maintainers.tailhook ];
     platforms = platforms.all;
   };
+};
 }
