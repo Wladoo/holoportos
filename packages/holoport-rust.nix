f6a23a6109b910dbf13c0e99743547bbc16248d5
samrose <samuel.rose@gmail.com>
2019-02-20 16:23:15 -0500

    holochain-rust

--- packages/holoport-rust.nix
+++ packages/holoport-rust.nix
@@ -1,20 +1,20 @@
 { stdenv, fetchFromGitHub, rustPlatform }:
 rustPlatform.buildRustPackage rec {
-  name = "hello_world";
+  name = "holochain-rust";
   version = "0.0.1";
 
   src = fetchFromGitHub {
-    owner = "samrose";
-    repo = "hello_world";
-    rev = "be6efdbdcb5dd10bf9da860a0882b34f94aac68a";
-    sha256 = "0blfx8m0sc4mlbmbw9m0z1sybn10sxpjqw6bhhnans86ywybgkaa";
+    owner = "holochain";
+    repo = "holochain-rust";
+    rev = "330af276015c02956ba4b3d1d65b032585778931";
+    sha256 = "10z21m18rwk1xa5d4zm77j5bp3b4vyvnqvb9hn8wbi2kqixlqnzr";
   };
-  #buildInputs = [ holo-rust ];
-  cargoSha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
+  buildInputs = [ zeromq4 ];
+  cargoSha256 = "06nvsllzv4qkyv1213qa566dfanpfb44mhp4n19w64hjw45qpc83";
 
   meta = with stdenv.lib; {
-    description = "a test";
-    homepage = https://github.com/samrose/hello_world;
+    description = "holochain-rust";
+    homepage = https://github.com/holochain/holochain-rust;
     license = licenses.unlicense;
     maintainers = [ maintainers.tailhook ];
     platforms = platforms.all;
