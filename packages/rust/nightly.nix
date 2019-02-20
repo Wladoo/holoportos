4e70bf3c7eb7d753bc8d00ad8a42df37a07fcc00
samrose <samuel.rose@gmail.com>
2019-02-20 14:46:07 -0500

    rustc nightly 2019-01-24

--- packages/rust/nightly.nix
+++ packages/rust/nightly.nix
@@ -8,13 +8,13 @@ rec {
   rustc = callPackage ./rustc.nix {
     inherit llvm targets targetPatches targetToolchains rustPlatform;
 
-    version = "nightly-2017-05-30";
+    version = "nightly-2019-01-24";
 
     configureFlags = [ "--release-channel=nightly" ];
 
     src = fetchurl {
-      url = "https://static.rust-lang.org/dist/2017-05-30/rustc-nightly-src.tar.gz";
-      sha256 = "90ce76db56a93f1b4532f2e62bbf12c243c4d156662b0d80c25319211ee7d0e0";
+      url = "https://static.rust-lang.org/dist/2019-01-24/rustc-nightly-src.tar.gz";
+      sha256 = "bf9715a05565040ab962a5d92d154d6842f60a68a6f463cd40b6660af0526c73";
     };
 
     patches = [
@@ -26,9 +26,9 @@ rec {
   };
 
   cargo = callPackage ./cargo.nix rec {
-    version = "0.18.0";
-    srcRev = "fe7b0cdcf5ca7aab81630706ce40b70f6aa2e666";
-    srcSha = "164iywv1l3v87b0pznf5kkzxigd6w19myv9d7ka4c65zgrk9n9px";
+    version = "1.33.0-nightly";
+    srcRev = "907c0febe7045fa02dff2a35c5e36d3bd59ea50d";
+    srcSha = "0i0afgbv09k6h167sc5l4grlwbyphbyfzq8i4pcr86x7c54z8z8a";
     depsSha256 = "1mrgd8ib48vxxbhkvsqqq4p19sc6b74x3cd8p6lhhlm6plrajrvm";
 
     inherit rustc; # the rustc that will be wrapped by cargo
