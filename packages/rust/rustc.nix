9d8007a6aebfa67f71ea8db7508adf82724e712c
samrose <samuel.rose@gmail.com>
2019-02-21 14:07:51 -0500

    configure flags

--- packages/rust/rustc.nix
+++ packages/rust/rustc.nix
@@ -46,9 +46,9 @@ stdenv.mkDerivation {
 
   # We need rust to build rust. If we don't provide it, configure will try to download it.
   configureFlags = configureFlags
-                ++ [ "--enable-local-rust" "--local-rust-root=${rustPlatform.rust.rustc}" "--enable-rpath" ]
+                ++ [ "--enable-local-rust" "--local-rust-root=${rustPlatform.rust}" "--enable-rpath" ]
                 ++ [ "--enable-vendor" "--disable-locked-deps" ]
-                ++ [ "--enable-vendor" ]
+                #++ [ "--enable-vendor" ]
                 ++ [ "--enable-llvm-link-shared" ]
                 # ++ [ "--jemalloc-root=${jemalloc}/lib"
                 ++ [ "--default-linker=${stdenv.cc}/bin/cc" ]
