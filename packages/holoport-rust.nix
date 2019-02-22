66e284ae62847eb65657bfeedeb4dc2a0bf11e2e
samrose <samuel.rose@gmail.com>
2019-02-22 16:27:51 -0500

    rustplatform

--- packages/holoport-rust.nix
+++ packages/holoport-rust.nix
@@ -22,13 +22,13 @@ let
 in
 rustPlatform.buildRustPackage rec {
   name = "hello_world";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = fetchFromGitHub {
     owner = "samrose";
     repo = "hello_world";
-    rev = "082775d0388552e32b644a1e3da8755a5f5e8c6a";
-    sha256 = "1jhkhpyh8g0ja6rr8bffphybhz53m7fav6r8pndv94msjdjdc7f6";
+    rev = "be6efdbdcb5dd10bf9da860a0882b34f94aac68a";
+    sha256 = "0blfx8m0sc4mlbmbw9m0z1sybn10sxpjqw6bhhnans86ywybgkaa";
   };
   #buildInputs = [ pkgs.rustc pkgs.cargo ];
   cargoSha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
