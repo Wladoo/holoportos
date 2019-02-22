4992e2e89803863fb77be7f09a1ff721b6e1d5fd
samrose <samuel.rose@gmail.com>
2019-02-22 11:34:32 -0500

    overlay in package

--- packages/holoport-rust.nix
+++ packages/holoport-rust.nix
@@ -8,16 +8,16 @@ let
   };
   rustOverlay = import "${rustOverlayRepo}/rust-overlay.nix";
   nixpkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
-  rust = let
-    channel = (nixpkgs.rustChannelOfTargets
+  rust = rec {
+
+    channels = (nixpkgs.rustChannelOfTargets
       "nightly"
       "2019-01-24"
       [ "x86_64-unknown-linux-gnu" "wasm32-unknown-unknown" ]
-  );
-  in {
-    rustc = channel.rust;
-    inherit (channel) cargo;
-    };
+     );
+    rustc = channels.rust;
+    inherit (channels) cargo;
+  };
   rustPlatform = recurseIntoAttrs (makeRustPlatform rust);
 in
 rustPlatform.buildRustPackage rec {
