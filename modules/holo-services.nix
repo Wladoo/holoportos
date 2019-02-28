1d0efe5ae9ad7e0a5d8c18fb672636bf1aa23451
samrose <samuel.rose@gmail.com>
2019-02-28 12:30:31 -0500

    a first cut of conductor services just to test

--- modules/holo-services.nix
+++ modules/holo-services.nix
@@ -2,11 +2,12 @@
 
 {
   stdenv.mkDerivation {
-  name = "conductor-fonfig";
-  src = fetchurl {
-    url = https://gist.githubusercontent.com/samrose/caba3e3556e11151bdc90f0b0fb567a9/raw/a46de503498dbe1bbed275168e3a6db459f235f0/empty-container.toml;
-    sha256 = "1g8m78qzdnbp3vp8sd5qlpa2p1r5fmav28yjsrf65k4wfkn62dr4";
-  };
+    name = "conductor-fonfig";
+    src = fetchurl {
+      url = https://gist.githubusercontent.com/samrose/caba3e3556e11151bdc90f0b0fb567a9/raw/a46de503498dbe1bbed275168e3a6db459f235f0/empty-container.toml;
+      sha256 = "1g8m78qzdnbp3vp8sd5qlpa2p1r5fmav28yjsrf65k4wfkn62dr4";
+    };
+  }
 
 
 
