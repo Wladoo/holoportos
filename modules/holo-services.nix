db95e90b818b48d65c2dc9aab88e6286d8c5f2a0
samrose <samuel.rose@gmail.com>
2019-02-28 14:47:41 -0500

    test known good service

--- modules/holo-services.nix
+++ modules/holo-services.nix
@@ -1,11 +1,11 @@
 { stdenv, pkgs, lib, config, fetchurl, ... }:
 
-{
+let
   conductor-config = fetchurl {
       url = https://gist.githubusercontent.com/samrose/caba3e3556e11151bdc90f0b0fb567a9/raw/a46de503498dbe1bbed275168e3a6db459f235f0/empty-container.toml;
       sha256 = "1g8m78qzdnbp3vp8sd5qlpa2p1r5fmav28yjsrf65k4wfkn62dr4";
     };
-
+in
    systemd.services.conductor = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
@@ -19,4 +19,3 @@
    };
 
    #environment.systemPackages = [ pkgs.screen ];
-}
