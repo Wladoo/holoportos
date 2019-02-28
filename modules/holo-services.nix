15c06120711b7c33ec5be41338fa275d7a286573
samrose <samuel.rose@gmail.com>
2019-02-28 13:52:42 -0500

    services

--- modules/holo-services.nix
+++ modules/holo-services.nix
@@ -1,20 +1,23 @@
 { stdenv, lib, config, fetchurl, ... }:
 
+with import <nixpkgs> {};
+
 let
-  conductor-config = stdenv.mkDerivation {
-    name = "conductor-fonfig";
-    src = fetchurl {
+  conductor-config = fetchurl {
       url = https://gist.githubusercontent.com/samrose/caba3e3556e11151bdc90f0b0fb567a9/raw/a46de503498dbe1bbed275168e3a6db459f235f0/empty-container.toml;
       sha256 = "1g8m78qzdnbp3vp8sd5qlpa2p1r5fmav28yjsrf65k4wfkn62dr4";
     };
-  }
+
 
 in
-
-  systemd.services.conductor = {
+with lib;
+{
+  services.conductor = {
+    description = "Holo Conductor Service";
     path = [pkgs.bash];
     serviceConfig = {
       ExecStart = "/run/current-system/sw/bin/holochain -c ${conductor-config}";
     };
     wantedBy = [ "default.target" ];
   };
+}
