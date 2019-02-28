dca6f6be0c1e491e0872cdbd41e61eb9416cbaae
samrose <samuel.rose@gmail.com>
2019-02-28 14:04:41 -0500

    test known good service

--- modules/holo-services.nix
+++ modules/holo-services.nix
@@ -1,25 +1,17 @@
 { stdenv, lib, config, fetchurl, ... }:
 
-with import <nixpkgs> {};
-
-let
-  conductor-config = fetchurl {
-      url = https://gist.githubusercontent.com/samrose/caba3e3556e11151bdc90f0b0fb567a9/raw/a46de503498dbe1bbed275168e3a6db459f235f0/empty-container.toml;
-      sha256 = "1g8m78qzdnbp3vp8sd5qlpa2p1r5fmav28yjsrf65k4wfkn62dr4";
-    };
-
-    cfg = config.services.conductor;
-in
-with lib;
 {
-   options = {
-     systemd.services.conductor = {
-       description = "Holo Conductor Service";
-       path = [pkgs.bash];
-       serviceConfig = {
-         ExecStart = "/run/current-system/sw/bin/holochain -c ${conductor-config}";
-       };
-       wantedBy = [ "default.target" ];
-     };
-  };
+   systemd.services.ircSession = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      description = "Start the irc client of username.";
+      serviceConfig = {
+        Type = "forking";
+        User = "holoportos";
+        ExecStart = ''${pkgs.screen}/bin/screen -dmS irc ${pkgs.irssi}/bin/irssi'';
+        ExecStop = ''${pkgs.screen}/bin/screen -S irc -X quit'';
+      };
+   };
+
+   environment.systemPackages = [ pkgs.screen ];
 }
