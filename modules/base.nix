0b069903567c2f531240e1c79b59cee37c1a5250
samrose <samuel.rose@gmail.com>
2019-03-05 15:50:07 -0500

    holo-led

--- modules/base.nix
+++ modules/base.nix
@@ -14,6 +14,7 @@ let
   });
 
   pre-net-led = pkgs.callPackage ../packages/pre-net-led/pre-net-led.nix {};
+  holo-led = pkgs.callPackage ../packages/holo-led/holo-led.nix {};
 in
 {
   options = {
