debca3667ed6cd21d4e3074ea72d1d876c311411
samrose <samuel.rose@gmail.com>
2019-03-11 16:57:50 -0400

    remove osquery

--- modules/base.nix
+++ modules/base.nix
@@ -137,14 +137,6 @@ in
           RemainAfterExit = "yes";
         };
       };
-      services.osquery.enable = true;
-      services.osquery.loggerPath = "/var/log/osquery/logs";
-      services.osquery.pidfile = "/var/run/osqueryd.pid";
-      services.osquery.extraConfig = {
-        disable_audit=false;
-        audit_allow_config=true;
-        audit_allow_process_events=true;
-      };# per https://osquery.readthedocs.io/en/2.6.1/deployment/process-auditing/
 
     })
   ];
