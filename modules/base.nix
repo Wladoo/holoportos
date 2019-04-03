ef177c5fc95e3df6d8ebbb086cef286ed953a317
samrose <samuel.rose@gmail.com>
2019-04-03 18:15:52 -0400

    yarn and yarn2nix packages add pkgs ref

--- modules/base.nix
+++ modules/base.nix
@@ -16,7 +16,7 @@ let
   holo-led = pkgs.callPackage ../packages/holo-led/holo-led.nix {};
   shutdown-led = pkgs.callPackage ../packages/shutdown-led/shutdown-led.nix {};
   holo-health = pkgs.callPackage ../packages/holo-health/holo-health.nix {};
-  yarn2nix = callPackage ../packages/yarn2nix/default.nix {};
+  yarn2nix = pkgs.callPackage ../packages/yarn2nix/default.nix {};
   hptest = pkgs.writeShellScriptBin "hptest" ''
     sudo lshw -C cpu >> hptest.txt
     sudo lshw -C memory >> hptest.txt
