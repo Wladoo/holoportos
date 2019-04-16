25bc3d0885715cecba15b60922115d2f04261365
samrose <samuel.rose@gmail.com>
2019-04-16 13:28:43 -0400

    pin uid gid

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -1,8 +1,8 @@
 # This is a list of all modules that should always be imported
 [
+  ./holochain.nix
   ./auto-update.nix
   ./base.nix
-  ./holochain.nix
   ./udiskie.nix
   ./user.nix
   ./util.nix
