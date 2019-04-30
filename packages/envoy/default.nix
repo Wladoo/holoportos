6f2e0bdda583763188c6955ebd14da1c987d2b39
samrose <samuel.rose@gmail.com>
2019-04-30 15:01:13 -0400

    reboot of envoy and hApps

--- packages/envoy/default.nix
+++ packages/envoy/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "envoy";
   src = fetchzip {
     url = https://github.com/samrose/envoy/archive/tmp-demo.zip;
-    sha256 = "1c4pa30rmpfaq2xfhzpg6sgv2aibhyw2z5drlxm4l67ssy95kzwn";
+    sha256 = "0hn1bwx0zz6r5x3bijb6hdrdsvvy7xb41dr4p8fwwgh60j7idnn5";
   };
   unpackPhase = ":";
   
