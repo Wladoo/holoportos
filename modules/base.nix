ea4e917b917491f2e9ef31fd9460d88265e48162
samrose <samuel.rose@gmail.com>
2019-05-06 14:27:01 -0400

    TMPDIR in environment of systemd service

--- modules/base.nix
+++ modules/base.nix
@@ -276,7 +276,7 @@ in
           after = [ "local-fs.target" "network.target" "systemd-activation.service" ];
           wantedBy = [ "multi-user.target" ];
           requires = [ "systemd-activation.service" ];
-          environment = { NIX_STORE = "/nix/store"; RUST_BACKTRACE = "full"; };
+          environment = { TMPDIR = "/home/holochain"; NIX_STORE = "/nix/store"; RUST_BACKTRACE = "full"; };
           serviceConfig = {
             ExecStart = ''/run/current-system/sw/bin/holochain -c /var/lib/holochain/conductor-config.toml'';
             #EnvironmentFile = "/etc/set-environment";
