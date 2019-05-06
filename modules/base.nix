6170ceca0233f0815bd633574abe61f67fb4df9a
samrose <samuel.rose@gmail.com>
2019-05-06 12:52:52 -0400

    set RUST_BACKTRACE env var for n3h

--- modules/base.nix
+++ modules/base.nix
@@ -276,7 +276,7 @@ in
           after = [ "local-fs.target" "network.target" "systemd-activation.service" ];
           wantedBy = [ "multi-user.target" ];
           requires = [ "systemd-activation.service" ];
-          environment = { NIX_STORE = "/nix/store"; RUST_BACKTRACE = "1"; };
+          environment = { NIX_STORE = "/nix/store"; RUST_BACKTRACE = "full"; };
           serviceConfig = {
             ExecStart = ''/run/current-system/sw/bin/holochain -c /var/lib/holochain/conductor-config.toml'';
             #Restart = "always";
